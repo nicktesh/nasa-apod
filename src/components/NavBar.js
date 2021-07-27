9eb410c8ad711e67b9d29ca3ecca82c86f20c7d9
Nick Tesh <NTesh@rspusa.local>
2021-07-27 11:13:51 -0400

    Nasa API Project

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+// src/components/NavBar.js
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <Link className="link" to="/" exact>
+          Take Me Home
+        </Link>
+      </ul>
+    </div>
+  );
+}
